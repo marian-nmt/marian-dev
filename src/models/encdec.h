27cdd3d8b4e054ea3785898a396d7262bb6439a5
Marcin Junczys-Dowmunt <marcinjd@microsoft.com>
2018-01-13 12:44:21 -0800

    right-to-left training, decoding, scoring

--- src/models/encdec.h
+++ src/models/encdec.h
@@ -255,6 +255,7 @@ public:
         "dec-cell-high-depth",
         "skip",
         "layer-normalization",
+        "right-left",
         "special-vocab",
         "tied-embeddings",
         "tied-embeddings-src",

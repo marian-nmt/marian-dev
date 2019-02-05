53d19815c86cc335a213df336716606c63d1099a
Marcin Junczys-Dowmunt <Marcin.JunczysDowmunt@microsoft.com>
2019-02-05 14:27:22 -0800

    comment out fake options

--- src/common/aliases.cpp
+++ src/common/aliases.cpp
@@ -38,6 +38,7 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
     config["enc-depth"] = 6;
     config["dec-depth"] = 6;
     config["dim-emb"] = 512;
+    config["tied-embeddings-all"] = true;
     config["transformer-dim-ffn"] = 2048;
     config["transformer-heads"] = 8;
     config["transformer-postprocess"] = "dan";
@@ -54,6 +55,7 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
     config["clip-norm"] = 5;
     config["sync-sgd"] = true;
     config["exponential-smoothing"] = 1e-4;
+    config["max-length"] = 100;
     config["mini-batch-fit"] = true;
     config["mini-batch"] = 1000;
     config["maxi-batch"] = 1000;
@@ -67,6 +69,7 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
     config["enc-depth"] = 6;
     config["dec-depth"] = 6;
     config["dim-emb"] = 1024;
+    config["tied-embeddings-all"] = true;
     config["transformer-dim-ffn"] = 4096;
     config["transformer-heads"] = 16;
     config["transformer-postprocess"] = "dan";
@@ -85,6 +88,7 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
     config["clip-norm"] = 5;
     config["sync-sgd"] = true;
     config["exponential-smoothing"] = 1e-4;
+    config["max-length"] = 100;
     config["mini-batch-fit"] = true;
     config["mini-batch"] = 1000;
     config["maxi-batch"] = 1000;
@@ -98,6 +102,7 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
     config["enc-depth"] = 12;
     config["dec-depth"] = 12;
     config["dim-emb"] = 768;
+    config["tied-embeddings-all"] = true;
     config["transformer-dim-ffn"] = 3072;
     config["transformer-heads"] = 12;
     config["transformer-postprocess"] = "dan";
@@ -112,12 +117,11 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
     config["bert-sep-symbol"] = "[SEP]";
     config["bert-class-symbol"] = "[CLS]";
     config["bert-masking-fraction"] = 0.15f;
+    config["input-types"] = std::vector<std::string>({"sequence", "class"});
 
     // BERT specific options - Next sentence classifier
     config["bert-train-type-embeddings"] = true;
     config["bert-type-vocab-size"] = 2;
-
-    //config["transformer-dropout-ffn"] = 0.1;
 
     // Training specific options
     config["learn-rate"] = 0.0002;
@@ -128,17 +132,19 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
     config["clip-norm"] = 5;
     config["sync-sgd"] = true;
     config["exponential-smoothing"] = 1e-4;
+    config["max-length"] = 512;
     config["mini-batch-fit"] = true;
     config["mini-batch"] = 1000;
     config["maxi-batch"] = 1000;
+    config["maxi-batch-sort"] = "src";
     config["workspace"] = 13000;
 
     // // Validation settings
     // config["valid-heldout"] = 5000;
     // config["valid-metrics"] = std::vector<std::string>({
     //   "ce-mean-words",
-    //   "bert-masked-lm-accuracy",
-    //   "bert-next-sentence-accuracy"
+    //   "bert-lm-accuracy",
+    //   "bert-sentence-accuracy"
     // });
     // config["valid-freq"] = 5000;
     // config["save-freq"] = 5000;

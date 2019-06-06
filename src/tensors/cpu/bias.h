41da0bd469160de135be3633e96161117f5998f1
Mateusz Chudyk <mateuszchudyk@gmail.com>
2019-06-06 18:01:51 +0100

    Simplify bias.h

--- src/tensors/cpu/bias.h
+++ src/tensors/cpu/bias.h
@@ -1,16 +1,22 @@
 #pragma once
 
+#include "3rd_party/intgemm/intrinsics.h"
 #include "tensors/tensor.h"
 #include "tensors/tensor_allocator.h"
 #include "tensors/tensor_operators.h"
 
-#include <emmintrin.h>
-#include <immintrin.h>
-#include <tmmintrin.h>
-#include <xmmintrin.h>
-
 namespace marian {
 namespace cpu {
+
+namespace {
+template <class Register> static inline Register loadu_ps(const float* mem_addr);
+template <> INTGEMM_SSE2 inline __m128 loadu_ps(const float* mem_addr) {
+  return _mm_loadu_ps(mem_addr);
+}
+template <> INTGEMM_AVX512BW inline __m512 loadu_ps(const float* mem_addr) {
+  return _mm512_loadu_ps(mem_addr);
+}
+}
 
 /* TODO: CPUID dispatch, maybe integrate with intgemm */
 
@@ -19,33 +25,26 @@ static void AddBias(marian::Tensor C, const marian::Tensor Bias) {
     float* y = C->data();
     const float* x = C->data();
     const float* bias = Bias->data();
+    const int m = C->shape().elements() / C->shape()[-1];
+    const int n = C->shape()[-1];
 
-    int m = C->shape().elements() / C->shape()[-1];
-    int n = C->shape()[-1];
 #ifdef __AVX512F__
-    int n16 = n & ~15;
+    using vec_t = __m512;
 #else
-    int n4 = (n / 4) * 4;
+    using vec_t = __m128;
 #endif
 
+    const int step = sizeof(vec_t) / 4; // 4 bytes per float
+    const int n_aligned = n & (step - 1);
     for(int j = 0; j < m; ++j) {
         int i = 0;
-#ifdef __AVX512F__
-        for (; i < n16; i += 16) {
-            __m512 ai = _mm512_loadu_ps(x + j * n + i);
-            __m512 bi = _mm512_loadu_ps(bias + i);
-            __m512 yi = _mm512_add_ps(ai, bi);
-            _mm512_storeu_ps(y + j * n + i, yi);
+        for (; i < n_aligned; i += step) {
+            auto ai = loadu_ps<vec_t>(x + j * n + i);
+            auto bi = loadu_ps<vec_t>(bias + i);
+            auto yi = intgemm::add_ps(ai, bi);
+            intgemm::storeu_ps(y + j * n + i, yi);
         }
-#else
-        for (; i < n4; i += 4) {
-            __m128 ai = _mm_loadu_ps(x + j * n + i);
-            __m128 bi = _mm_loadu_ps(bias + i);
-            __m128 yi = _mm_add_ps(ai, bi);
-            _mm_storeu_ps(y + j * n + i, yi);
-        }
-#endif
-        for (; i < n; i++) {
+        for (; i < n; ++i) {
             y[j * n + i] = x[j * n + i] + bias[i];
         }
     }

148718445e111c186edc0f1d63f693a71d316acd
Mateusz Chudyk <mateuszchudyk@gmail.com>
2019-06-21 15:46:34 +0100

    Refactor dispatch.h

--- src/tensors/dispatch.h
+++ src/tensors/dispatch.h
@@ -1,251 +1,65 @@
 #pragma once
 
+#define TRANSFORM_LIST_1(transform, head) transform(1, head)
+#define TRANSFORM_LIST_2(transform, head, ...) transform(2, head), TRANSFORM_LIST_1(transform, __VA_ARGS__)
+#define TRANSFORM_LIST_3(transform, head, ...) transform(3, head), TRANSFORM_LIST_2(transform, __VA_ARGS__)
+#define TRANSFORM_LIST_4(transform, head, ...) transform(4, head), TRANSFORM_LIST_3(transform, __VA_ARGS__)
+#define TRANSFORM_LIST_5(transform, head, ...) transform(5, head), TRANSFORM_LIST_4(transform, __VA_ARGS__)
+#define TRANSFORM_LIST_6(transform, head, ...) transform(6, head), TRANSFORM_LIST_5(transform, __VA_ARGS__)
+#define TRANSFORM_LIST_7(transform, head, ...) transform(7, head), TRANSFORM_LIST_6(transform, __VA_ARGS__)
+#define TRANSFORM_LIST_8(transform, head, ...) transform(8, head), TRANSFORM_LIST_7(transform, __VA_ARGS__)
+#define TRANSFORM_LIST_9(transform, head, ...) transform(9, head), TRANSFORM_LIST_8(transform, __VA_ARGS__)
+#define TRANSFORM_LIST(n, transform, ...) TRANSFORM_LIST_##n(transform, __VA_ARGS__)
+
+#define MAKE_ARG_NAME(i, type) arg##i
+#define MAKE_ARG_TYPE_NAME(i, type) type MAKE_ARG_NAME(i, type)
+
+#define ARGS_NAMES(n, ...) TRANSFORM_LIST(n, MAKE_ARG_NAME, __VA_ARGS__)
+#define ARGS_TYPES_NAMES(n, ...) TRANSFORM_LIST(n, MAKE_ARG_TYPE_NAME, __VA_ARGS__)
+
 #ifdef CUDA_FOUND
 
-#define DISPATCH1(Function, Arg1)                                 \
+#define DISPATCH(function, n, ...)                                \
   namespace gpu {                                                 \
-  void Function(Arg1);                                            \
+    void function(__VA_ARGS__);                                   \
   }                                                               \
   namespace cpu {                                                 \
-  void Function(Arg1);                                            \
+    void function(__VA_ARGS__);                                   \
   }                                                               \
-  void Function(Arg1 arg1) {                                      \
+  static inline void function(ARGS_TYPES_NAMES(n, __VA_ARGS__)) { \
     if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu) \
-      gpu::Function(arg1);                                        \
+      gpu::function(ARGS_NAMES(n, __VA_ARGS__));                  \
     else                                                          \
-      cpu::Function(arg1);                                        \
-  }
-
-#define DISPATCH2(Function, Arg1, Arg2)                           \
-  namespace gpu {                                                 \
-  void Function(Arg1, Arg2);                                      \
-  }                                                               \
-  namespace cpu {                                                 \
-  void Function(Arg1, Arg2);                                      \
-  }                                                               \
-  static inline void Function(Arg1 arg1, Arg2 arg2) {             \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu) \
-      gpu::Function(arg1, arg2);                                  \
-    else                                                          \
-      cpu::Function(arg1, arg2);                                  \
-  }
-
-#define DISPATCH3(Function, Arg1, Arg2, Arg3)                     \
-  namespace gpu {                                                 \
-  void Function(Arg1, Arg2, Arg3);                                \
-  }                                                               \
-  namespace cpu {                                                 \
-  void Function(Arg1, Arg2, Arg3);                                \
-  }                                                               \
-  static inline void Function(Arg1 arg1, Arg2 arg2, Arg3 arg3) {  \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu) \
-      gpu::Function(arg1, arg2, arg3);                            \
-    else                                                          \
-      cpu::Function(arg1, arg2, arg3);                            \
-  }
-
-#define DISPATCH4(Function, Arg1, Arg2, Arg3, Arg4)                         \
-  namespace gpu {                                                           \
-  void Function(Arg1, Arg2, Arg3, Arg4);                                    \
-  }                                                                         \
-  namespace cpu {                                                           \
-  void Function(Arg1, Arg2, Arg3, Arg4);                                    \
-  }                                                                         \
-  static inline void Function(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) { \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu)           \
-      gpu::Function(arg1, arg2, arg3, arg4);                                \
-    else                                                                    \
-      cpu::Function(arg1, arg2, arg3, arg4);                                \
-  }
-
-#define DISPATCH5(Function, Arg1, Arg2, Arg3, Arg4, Arg5)         \
-  namespace gpu {                                                 \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5);                    \
-  }                                                               \
-  namespace cpu {                                                 \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5);                    \
-  }                                                               \
-  static inline void Function(                                    \
-      Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5) {    \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu) \
-      gpu::Function(arg1, arg2, arg3, arg4, arg5);                \
-    else                                                          \
-      cpu::Function(arg1, arg2, arg3, arg4, arg5);                \
-  }
-
-#define DISPATCH6(Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)           \
-  namespace gpu {                                                         \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6);                      \
-  }                                                                       \
-  namespace cpu {                                                         \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6);                      \
-  }                                                                       \
-  static inline void Function(                                            \
-      Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6) { \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu)         \
-      gpu::Function(arg1, arg2, arg3, arg4, arg5, arg6);                  \
-    else                                                                  \
-      cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6);                  \
-  }
-
-#define DISPATCH7(Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7) \
-  namespace gpu {                                                     \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7);            \
-  }                                                                   \
-  namespace cpu {                                                     \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7);            \
-  }                                                                   \
-  static inline void Function(Arg1 arg1,                              \
-                              Arg2 arg2,                              \
-                              Arg3 arg3,                              \
-                              Arg4 arg4,                              \
-                              Arg5 arg5,                              \
-                              Arg6 arg6,                              \
-                              Arg7 arg7) {                            \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu)     \
-      gpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7);        \
-    else                                                              \
-      cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7);        \
-  }
-
-#define DISPATCH8(Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8) \
-  namespace gpu {                                                           \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8);            \
-  }                                                                         \
-  namespace cpu {                                                           \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8);            \
-  }                                                                         \
-  static inline void Function(Arg1 arg1,                                    \
-                              Arg2 arg2,                                    \
-                              Arg3 arg3,                                    \
-                              Arg4 arg4,                                    \
-                              Arg5 arg5,                                    \
-                              Arg6 arg6,                                    \
-                              Arg7 arg7,                                    \
-                              Arg8 arg8) {                                  \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu)           \
-      gpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);        \
-    else                                                                    \
-      cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);        \
-  }
-
-#define DISPATCH9(                                                         \
-    Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9)        \
-  namespace gpu {                                                          \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9);     \
-  }                                                                        \
-  namespace cpu {                                                          \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9);     \
-  }                                                                        \
-  static inline void Function(Arg1 arg1,                                   \
-                              Arg2 arg2,                                   \
-                              Arg3 arg3,                                   \
-                              Arg4 arg4,                                   \
-                              Arg5 arg5,                                   \
-                              Arg6 arg6,                                   \
-                              Arg7 arg7,                                   \
-                              Arg8 arg8,                                   \
-                              Arg9 arg9) {                                 \
-    if(arg1->getBackend()->getDeviceId().type == DeviceType::gpu)          \
-      gpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9); \
-    else                                                                   \
-      cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9); \
+      cpu::function(ARGS_NAMES(n, __VA_ARGS__));                  \
   }
 
 #else
 
-#define DISPATCH1(Function, Arg1) \
-  namespace cpu {                 \
-  void Function(Arg1);            \
-  }                               \
-  void Function(Arg1 arg1) { cpu::Function(arg1); }
-
-#define DISPATCH2(Function, Arg1, Arg2)               \
-  namespace cpu {                                     \
-  void Function(Arg1, Arg2);                          \
-  }                                                   \
-  static inline void Function(Arg1 arg1, Arg2 arg2) { \
-    cpu::Function(arg1, arg2);                        \
-  }
-
-#define DISPATCH3(Function, Arg1, Arg2, Arg3)                    \
-  namespace cpu {                                                \
-  void Function(Arg1, Arg2, Arg3);                               \
-  }                                                              \
-  static inline void Function(Arg1 arg1, Arg2 arg2, Arg3 arg3) { \
-    cpu::Function(arg1, arg2, arg3);                             \
-  }
-
-#define DISPATCH4(Function, Arg1, Arg2, Arg3, Arg4)                         \
-  namespace cpu {                                                           \
-  void Function(Arg1, Arg2, Arg3, Arg4);                                    \
-  }                                                                         \
-  static inline void Function(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) { \
-    cpu::Function(arg1, arg2, arg3, arg4);                                  \
-  }
-
-#define DISPATCH5(Function, Arg1, Arg2, Arg3, Arg4, Arg5)      \
-  namespace cpu {                                              \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5);                 \
-  }                                                            \
-  static inline void Function(                                 \
-      Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5) { \
-    cpu::Function(arg1, arg2, arg3, arg4, arg5);               \
-  }
-
-#define DISPATCH6(Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)           \
-  namespace cpu {                                                         \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6);                      \
-  }                                                                       \
-  static inline void Function(                                            \
-      Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6) { \
-    cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6);                    \
-  }
-
-#define DISPATCH7(Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7) \
-  namespace cpu {                                                     \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7);            \
-  }                                                                   \
-  static inline void Function(Arg1 arg1,                              \
-                              Arg2 arg2,                              \
-                              Arg3 arg3,                              \
-                              Arg4 arg4,                              \
-                              Arg5 arg5,                              \
-                              Arg6 arg6,                              \
-                              Arg7 arg7) {                            \
-    cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7);          \
-  }
-
-#define DISPATCH8(Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8) \
-  namespace cpu {                                                           \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8);            \
-  }                                                                         \
-  static inline void Function(Arg1 arg1,                                    \
-                              Arg2 arg2,                                    \
-                              Arg3 arg3,                                    \
-                              Arg4 arg4,                                    \
-                              Arg5 arg5,                                    \
-                              Arg6 arg6,                                    \
-                              Arg7 arg7,                                    \
-                              Arg8 arg8) {                                  \
-    cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);          \
-  }
-
-#define DISPATCH9(                                                       \
-    Function, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9)      \
-  namespace cpu {                                                        \
-  void Function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9);   \
-  }                                                                      \
-  static inline void Function(Arg1 arg1,                                 \
-                              Arg2 arg2,                                 \
-                              Arg3 arg3,                                 \
-                              Arg4 arg4,                                 \
-                              Arg5 arg5,                                 \
-                              Arg6 arg6,                                 \
-                              Arg7 arg7,                                 \
-                              Arg8 arg8,                                 \
-                              Arg9 arg9) {                               \
-    cpu::Function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9); \
+#define DISPATCH(function, n, ...)                                \
+  namespace cpu {                                                 \
+    void function(__VA_ARGS__);                                   \
+  }                                                               \
+  static inline void function(ARGS_TYPES_NAMES(n, __VA_ARGS__)) { \
+    cpu::function(ARGS_NAMES(n, __VA_ARGS__));                    \
   }
 
 #endif
+
+#define DISPATCH1(function, type1) \
+  DISPATCH(function, 1, type1)
+#define DISPATCH2(function, type1, type2) \
+  DISPATCH(function, 2, type1, type2)
+#define DISPATCH3(function, type1, type2, type3) \
+  DISPATCH(function, 3, type1, type2, type3)
+#define DISPATCH4(function, type1, type2, type3, type4) \
+  DISPATCH(function, 4, type1, type2, type3, type4)
+#define DISPATCH5(function, type1, type2, type3, type4, type5) \
+  DISPATCH(function, 5, type1, type2, type3, type4, type5)
+#define DISPATCH6(function, type1, type2, type3, type4, type5, type6) \
+  DISPATCH(function, 6, type1, type2, type3, type4, type5, type6)
+#define DISPATCH7(function, type1, type2, type3, type4, type5, type6, type7) \
+  DISPATCH(function, 7, type1, type2, type3, type4, type5, type6, type7)
+#define DISPATCH8(function, type1, type2, type3, type4, type5, type6, type7, type8) \
+  DISPATCH(function, 8, type1, type2, type3, type4, type5, type6, type7, type8)
+#define DISPATCH9(function, type1, type2, type3, type4, type5, type6, type7, type8, type9) \
+  DISPATCH(function, 9, type1, type2, type3, type4, type5, type6, type7, type8, type9)

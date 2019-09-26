ba7a2602b7f9c0001d1a89b3d747365578486b5d
Ulrich Germann <ugermnann@inf.ed.ac.uk>
2019-09-26 22:23:02 +0000

    Code refactoring.

---
+++ src/server/api/rapidjson_utils.cpp
@@ -0,0 +1,14 @@
+#include "rapidjson_utils.h"
+namespace rapidjson {
+
+  Value*
+  ensure_path(Value& node, MemoryPoolAllocator<>& alloc, char const* key){
+    if (!node.IsObject())
+      return NULL;
+    auto m = node.FindMember(key);
+    if (m != node.MemberEnd())
+      return &(m->value);
+    return &(m->value.AddMember(StringRef(key),{},alloc)[key]);
+  }
+
+}

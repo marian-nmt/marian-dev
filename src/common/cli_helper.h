37515bf7e5b53453307517a9850d439021f46b64
Ulrich Germann <ugermann@inf.ed.ac.uk>
2020-02-06 21:12:22 +0000

    Bug fix.

--- src/common/cli_helper.h
+++ src/common/cli_helper.h
@@ -14,8 +14,8 @@ static inline std::string InterpolateEnvVars(std::string str) {
   // temporary workaround for MS-internal PhillyOnAzure cluster: warm storage
   // presently has the form /hdfs/VC instead of /{gfs,hdfs}/CLUSTER/VC
 
-  // Catch stdin/stdout and do not process
-  std::cerr << str << std::endl;
+  // Catch stdin/stdout and do not process // Why is this extra check necessary?
+  // std::cerr << str << std::endl;
   if(str == "stdin" || str == "stdout") {
     return str;
   }

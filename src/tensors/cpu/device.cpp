4484219152d9dcb40bf638a8f3e499e384d19c79
Nikolay Bogoychev <nheart@gmail.com>
2020-02-11 16:02:18 +0000

    Refactor cpu allocation code. Do not fallback to malloc

--- src/tensors/cpu/device.cpp
+++ src/tensors/cpu/device.cpp
@@ -10,26 +10,19 @@ namespace marian {
 namespace cpu {
 
 // allocate function for tensor reserve() below. 
-// Needed for AVX512, while not available on all compilers. It seems clang
-// does not have aligned_alloc for all cstlib versions. If AVX512 is not used
-// a simple malloc is probably fine. 
-// Should generate a runtime error otherwise as we have a check in the AVX512 
-// functions which tests for alignment. 
+// Alignment is needed because we use AVX512 and AVX2 vectors. We should fail if we can't allocate aligned memory.
 #ifdef _WIN32
 #define MALLOC(size) _aligned_malloc(size, alignment_)
-#elif __APPLE__
-//On macos, aligned_alloc is available only on c++17
-//Furthermore, it requires that the memory requested is an exact multiple of the alignment, otherwise it fails
-void * apple_aligned_alloc(size_t alignment_, size_t size) {
+#else
+// On macos, aligned_alloc is available only on c++17
+// Furthermore, it requires that the memory requested is an exact multiple of the alignment, otherwise it fails.
+// posix_memalign is available both Mac (Since 2016) and Linux and in both gcc and clang
+void * posix_aligned_alloc(size_t alignment_, size_t size) {
     void * mem_;
     posix_memalign(&mem_, alignment_, size);
     return mem_;
 }
-#define MALLOC(size) apple_aligned_alloc(alignment_, size)
-#elif __GNUC__
-#define MALLOC(size) aligned_alloc(alignment_, size)
-#else
-#define MALLOC(size) malloc(size)
+#define MALLOC(size) posix_aligned_alloc(alignment_, size)
 #endif
 
 #ifdef _WIN32
@@ -47,14 +40,12 @@ void Device::reserve(size_t size) {
   ABORT_IF(size < size_ || size == 0,
            "New size must be larger than old size and larger than 0");
 
+  uint8_t *temp = static_cast<uint8_t*>(MALLOC(size));
   if(data_) {
-    uint8_t *temp = static_cast<uint8_t*>(MALLOC(size));
     std::copy(data_, data_ + size_, temp);
     FREE(data_);
-    data_ = temp;
-  } else {
-    data_ = static_cast<uint8_t*>(MALLOC(size));
   }
+  data_ = temp;
   size_ = size;
 }
 }  // namespace cpu

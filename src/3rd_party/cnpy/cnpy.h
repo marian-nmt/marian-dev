b7330e21c75ea901dbc011279834a4c9bf5e48dc
Marcin Junczys-Dowmunt <marcinjd@microsoft.com>
2020-04-12 17:20:14 -0700

    add type support for npz

--- src/3rd_party/cnpy/cnpy.h
+++ src/3rd_party/cnpy/cnpy.h
@@ -5,16 +5,18 @@
 #ifndef LIBCNPY_H_
 #define LIBCNPY_H_
 
-#include<string>
-#include<stdexcept>
-#include<sstream>
-#include<vector>
-#include<cstdio>
-#include<typeinfo>
-#include<iostream>
-#include<cassert>
-#include<zlib.h>
-#include<map>
+// use included zlib
+#include "zlib/zlib.h"
+
+#include <string>
+#include <stdexcept>
+#include <sstream>
+#include <vector>
+#include <cstdio>
+#include <typeinfo>
+#include <iostream>
+#include <cassert>
+#include <map>
 #include <memory>
 
 namespace cnpy {
@@ -22,6 +24,7 @@ namespace cnpy {
     struct NpyArray {
         std::vector<char> bytes;
         std::vector<unsigned int> shape;
+        char type;
         unsigned int word_size{1};
         bool fortran_order{0};
 
@@ -51,7 +54,7 @@ namespace cnpy {
     char map_type(const std::type_info& t);
     static inline std::vector<char> create_npy_header(char type, size_t word_size, const unsigned int* shape, const unsigned int ndims);
     template<typename T> std::vector<char> create_npy_header(const T* data, const unsigned int* shape, const unsigned int ndims);
-    void parse_npy_header(FILE* fp,unsigned int& word_size, unsigned int*& shape, unsigned int& ndims, bool& fortran_order);
+    void parse_npy_header(FILE* fp, char& type, unsigned int& word_size, unsigned int*& shape, unsigned int& ndims, bool& fortran_order);
     void parse_zip_footer(FILE* fp, unsigned short& nrecs, unsigned int& global_header_size, unsigned int& global_header_offset);
     npz_t npz_load(std::string fname);
     NpyArrayPtr npz_load(std::string fname, std::string varname);
@@ -84,9 +87,10 @@ namespace cnpy {
         if(fp) {
             //file exists. we need to append to it. read the header, modify the array size
             unsigned int word_size, tmp_dims;
+            char type;
             unsigned int* tmp_shape = 0;
             bool fortran_order;
-            parse_npy_header(fp,word_size,tmp_shape,tmp_dims,fortran_order);
+            parse_npy_header(fp,type,word_size,tmp_shape,tmp_dims,fortran_order);
             assert(!fortran_order);
 
             if(word_size != sizeof(T)) {

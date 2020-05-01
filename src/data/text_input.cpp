455724d3b9526933d8590eb8ea776e08efa9b268
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2020-05-01 16:10:27 +0100

    Add function converting multi-line tab-separated textual input

--- src/data/text_input.cpp
+++ src/data/text_input.cpp
@@ -27,7 +27,7 @@ TextInput::TextInput(std::vector<std::string> inputs,
       vocabs_(vocabs),
       maxLength_(options_->get<size_t>("max-length")),
       maxLengthCrop_(options_->get<bool>("max-length-crop")) {
-  // note: inputs are automatically stored in the inherited variable named paths_, but these ar
+  // Note: inputs are automatically stored in the inherited variable named paths_, but these are
   // texts not paths!
   for(const auto& text : paths_)
     files_.emplace_back(new std::istringstream(text));
@@ -47,6 +47,7 @@ SentenceTuple TextInput::next() {
       Words words = vocabs_[i]->encode(line, /*addEOS =*/ true, /*inference =*/ inference_);
       if(this->maxLengthCrop_ && words.size() > this->maxLength_) {
         words.resize(maxLength_);
+        words.back() = vocabs_.back()->getEosId();  // note: this will not work with class-labels
       }
       if(words.empty())
         words.push_back(Word::ZERO); // @TODO: What is this for? @BUGBUG: addEOS=true, so this can never happen, right?

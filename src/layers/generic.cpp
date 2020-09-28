9919bb881165719e15411218093cb17d42c4f52a
Nikolay Bogoychev <nheart@gmail.com>
2020-09-28 13:56:59 +0000

    Avoid unnecessary transpose by using intgemm::PrepareBTransposed

--- src/layers/generic.cpp
+++ src/layers/generic.cpp
@@ -286,26 +286,20 @@ namespace marian {
 #if COMPILE_CPU
         // Shortlisting with intgemm. We either get float32 Wt_ or intgemm formatted Wt_ (in future implementation potentially)
         // The two cases do exactly the same, with the difference that the first case is for 8bit integers and the second is for 16bit integers
+        bool transposed = !isLegacyUntransposedW;
         if ((graph_->getBackend()->isInt8() || matchType<intgemm8>(Wt_->value_type()) )&& graph_->getDeviceId().type == DeviceType::cpu) {
-          if (!isLegacyUntransposedW) { // We expect Wt_ to be untransposed, as our prepareB works on untransposed matrices.
-            Wt_ = transpose(Wt_);
-            isLegacyUntransposedW = true;
-          }
           if (isIntgemm(Wt_->value_type())) { // If we already have intgemm formatted matrix, just select columns from it. Intgemm equivalent of index_select
+            std::cerr << "Are we ever here" << std::endl;
             cachedShortWt_ = marian::cpu::integer::selectColumnsB<Type::int8>(Wt_, shortlist_->indices(), -1000.0 /*clip_value currently unused */);
           } else { // Else, convert the Wt_ matrix to intgemm format and then select vocabulary items from it.
-            cachedShortWt_ = marian::cpu::integer::prepareB<Type::int8>(Wt_, marian::cpu::integer::quantMult<Type::int8>(Wt_), -1000.0 /*clip_value currently unused */);
+            cachedShortWt_ = marian::cpu::integer::prepareB<Type::int8>(Wt_, marian::cpu::integer::quantMult<Type::int8>(Wt_), -1000.0 /*clip_value currently unused */, transposed /*Use different routine as Wt is transposed*/);
             cachedShortWt_ = marian::cpu::integer::selectColumnsB<Type::int8>(cachedShortWt_, shortlist_->indices(), -1000.0 /*clip_value currently unused */);
           }
         } else if ((graph_->getBackend()->isInt16() || matchType<intgemm16>(Wt_->value_type()) )&& graph_->getDeviceId().type == DeviceType::cpu) {
-          if (!isLegacyUntransposedW) {
-            Wt_ = transpose(Wt_);
-            isLegacyUntransposedW = true;
-          }
           if (isIntgemm(Wt_->value_type())) {
             cachedShortWt_ = marian::cpu::integer::selectColumnsB<Type::int16>(Wt_, shortlist_->indices(), -1000.0 /*clip_value currently unused */);
           } else {
-            cachedShortWt_ = marian::cpu::integer::prepareB<Type::int16>(Wt_, marian::cpu::integer::quantMult<Type::int16>(Wt_), -1000.0 /*clip_value currently unused */);
+            cachedShortWt_ = marian::cpu::integer::prepareB<Type::int16>(Wt_, marian::cpu::integer::quantMult<Type::int16>(Wt_), -1000.0 /*clip_value currently unused */, transposed /*Use different routine as Wt is transposed*/);
             cachedShortWt_ = marian::cpu::integer::selectColumnsB<Type::int16>(cachedShortWt_, shortlist_->indices(), -1000.0 /*clip_value currently unused */);
           }
         } else {
--- src/tensors/cpu/intgemm_interface.h
+++ src/tensors/cpu/intgemm_interface.h
@@ -80,13 +80,18 @@ template<Type vtype>
 struct PrepareBNodeOp : public NaryNodeOp {
 float clipValue_;
 float quantMult_;
-
-  PrepareBNodeOp(Expr input, Expr quant_mult, float clipValue)
-      : NaryNodeOp({input, quant_mult}, input->shape(), intgemm_<vtype>::intgemmType), clipValue_(clipValue) {
+bool transposed_; /*This is only used for the output layer which has a different layout from
+                   *all other matrices in the code. By default we shouldn't do an extra transpose.*/
+  PrepareBNodeOp(Expr input, Expr quant_mult, float clipValue, bool transposed=false)
+      : NaryNodeOp({input, quant_mult}, newShape(input, transposed), intgemm_<vtype>::intgemmType), clipValue_(clipValue), transposed_(transposed) {
 
     set_name(input->name());
     // Check if arguments are not null
-    ABORT_IF(input->shape()[-1] %8 != 0, "Columns of matrix: " + input->type() + " must be multiple of 8.");
+    if (!transposed_) {
+      ABORT_IF(input->shape()[-1] %8 != 0, "Columns of matrix: " + input->type() + " must be multiple of 8.");
+    } else {
+      ABORT_IF((input->shape().elements()/input->shape()[-1]) %8 != 0, "Rows of matrix: " + input->type() + " must be multiple of 8.");
+    }
   }
 
   NodeOps forwardOps() override {
@@ -95,12 +100,18 @@ float quantMult_;
       typedef typename intgemm_<vtype>::type Integer;
       if (isIntgemm(child(0)->value_type())) {
         val_ = child(0)->val();
-      } else {
+      } else if (!transposed_) {
         intgemm_<vtype>::width::PrepareB(child(0)->val()->data(), /*input*/
                                       val_->data<Integer>(), /*output*/
                                       *child(1)->val()->data(), /*Quant Mult*/
                                       rows(child(0)->val()),
                                       cols(child(0)->val()));
+      } else {
+        intgemm_<vtype>::width::PrepareBTransposed(child(0)->val()->data(), /*input*/
+                                      val_->data<Integer>(), /*output*/
+                                      *child(1)->val()->data(), /*Quant Mult*/
+                                      cols(child(0)->val()), /*Cols and rows need to be swapped*/
+                                      rows(child(0)->val())); /*Cols and rows need to be swapped*/
       }
     )};
   }
@@ -111,6 +122,17 @@ float quantMult_;
   }
 
   const std::string type() override { return "intgemmPrepareB"; }
+
+  static Shape newShape(Expr input, bool transposed) {
+    Shape ret = input->shape();
+    if (transposed) {
+      ret.set(0, input->shape()[-1]);
+      ret.set(1, input->shape()[0]);
+    } else {
+      ret = input->shape();
+    }
+    return ret;
+  }
 };
 
 /*
@@ -128,6 +150,7 @@ struct SelectColumnsBNodeOp : public UnaryNodeOp {
 public:
   float clipValue_;
   float quantMult_;
+  std::vector<uint_least32_t> indices_;
   SelectColumnsBNodeOp(Expr input, const std::vector<uint_least32_t>  &indices, float clipValue)
       : UnaryNodeOp(input, newShape(input, indices), intgemm_<vtype>::intgemmType), clipValue_(clipValue), indices_(indices) {
 
@@ -184,8 +207,6 @@ private:
     ret.set(1, indices.size());
     return ret;
   }
-
-  std::vector<uint_least32_t> indices_;
 };
 
 /*
@@ -421,8 +442,8 @@ static inline Expr prepareA(Expr a, Expr quantMult, float clipValue, bool shifte
 }
 
 template<Type vtype>
-static inline Expr prepareB(Expr b, Expr quantMult, float clipValue) {
-  return Expression<PrepareBNodeOp<vtype> >(b, quantMult, clipValue);
+static inline Expr prepareB(Expr b, Expr quantMult, float clipValue, bool transposed=false) {
+  return Expression<PrepareBNodeOp<vtype> >(b, quantMult, clipValue, transposed);
 }
 
 template<Type vtype>
@@ -444,11 +465,10 @@ static inline Expr affine(Expr a, Expr b, Expr bias, bool transA, bool transB, f
   Expr bQuant = nullptr;
   if (isIntgemm(bElementType)) {
     // This is the case where we already run SelectColumnB or we loaded a prepacked model.
-    // We ignore a transpose argument here, because we do not support it.
-    ABORT_IF(transB, "Transpose on prepareB not currently supported");
+    // We ignore a transpose argument here, because we should have already transposed the matrix!
     bQuant = b;
   } else {
-    bQuant = prepareB<vtype>(transB ? transpose(b) : b, bQuantMult, scale);
+    bQuant = prepareB<vtype>(b, bQuantMult, scale, transB);
   }
   if (shiftedBias) {
     // Shifted version of intgemm requires a specially prepared bias.

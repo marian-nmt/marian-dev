bd25b610fd0a1e1b96ec973ae25d15d9bcc87aa1
Rawn Henry <rhenry@nvidia.com>
2020-10-09 17:23:40 -0700

    Update license in prod.cu

--- src/tensors/gpu/prod.cu
+++ src/tensors/gpu/prod.cu
@@ -1,3 +1,8 @@
+/* All or part of this file was contributed by NVIDIA under license:
+ *   Copyright (C) 2020 NVIDIA Corporation
+ *   SPDX-License-Identifier: MIT
+ */
+
 #include <stdint.h>
 #include "tensors/tensor.h"
 #include "tensors/gpu/cuda_helpers.h"

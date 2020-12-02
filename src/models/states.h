6c11ba8477e576c9aa02bed37a4c195090201a2f
Rawn Henry <rhenry@nvidia.com>
2020-12-02 12:17:29 -0800

    Adds licenses to files changed

--- src/models/states.h
+++ src/models/states.h
@@ -1,3 +1,8 @@
+/* All or part of this file was contributed by NVIDIA under license:
+ *   Copyright (C) 2020 NVIDIA Corporation
+ *   SPDX-License-Identifier: MIT
+ */
+
 #pragma once
 
 #include "marian.h"

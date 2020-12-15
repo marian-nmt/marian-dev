764d7b85a15f10094444811c9609478022344926
Rawn Henry <rhenry@nvidia.com>
2020-12-15 11:05:06 -0800

    Fixes windows compile errors

--- src/tensors/cpu/prod.cpp
+++ src/tensors/cpu/prod.cpp
@@ -14,19 +14,18 @@ namespace marian {
 
 namespace cpu {
 
-void Affine(marian::Tensor C,
-            Ptr<Allocator> allocator,
-            const marian::Tensor& A,
-            const marian::Tensor& B,
-            const marian::Tensor& bias,
-            bool transA,
-            bool transB,
-            float beta,
-            float scalar,
-            bool relu_postprocess) {
-
-    ABORT("Not supported on CPU");
-  }
+void Affine(marian::Tensor /*C*/,
+            Ptr<Allocator> /*allocator*/,
+            const marian::Tensor& /*A*/,
+            const marian::Tensor& /*B*/,
+            const marian::Tensor& /*bias*/,
+            bool /*transA*/,
+            bool /*transB*/,
+            float /*beta*/,
+            float /*scalar*/,
+            bool /*relu_postprocess*/) {
+  ABORT("Not supported on CPU");
+}
 
 void Prod(marian::Tensor C,
           const marian::Tensor& A,

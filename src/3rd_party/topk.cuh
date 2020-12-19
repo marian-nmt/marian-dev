808d2f4d455264da5cb40a857125ff375de2d51c
Rawn Henry <rhenry@nvidia.com>
2020-12-18 16:44:23 -0800

    Adds comments to topk_stage1

--- src/3rd_party/topk.cuh
+++ src/3rd_party/topk.cuh
@@ -95,75 +95,135 @@ struct TopK {
   }
 };
 
+/// A binary reduction functor that CUB uses to perform the block reduce. This version is used when we want to find the max
+/// value in a given range.
 template<typename IndexType, typename T>
 __device__ __forceinline__ TopK<IndexType, T> reduce_topk_max(const TopK<IndexType, T>& a, const TopK<IndexType, T>& b) {
   return a.value > b.value ? a : b;
 }
 
+/// A binary reduction functor that CUB uses to perform the block reduce. This version is used when we want to find the min
+/// value in a given range.
 template<typename IndexType, typename T>
 __device__ __forceinline__ TopK<IndexType, T> reduce_topk_min(const TopK<IndexType, T>& a, const TopK<IndexType, T>& b) {
   return a.value < b.value ? a : b;
 }
 
-template<typename IndexType, typename T, int BLOCK_SIZE_, int BLOCKS_PER_BEAM_, bool getRowOffsets>
-__global__ void topk_stage_1(T* log_probs, 
+
+
+/**
+  This function performs the first phase of the topk. It finds the k largest elements within each BLOCKS_PER_ITEM_ blocks 
+  for each item in a row  and writes the indices to the topk_tmp_id_buf buffer and the values to the topk_tmp_val_buf.
+
+  These intermediate values are further reduced in the second phase of this kernel.
+
+  Note - This implementation modifies the input array in place but fixes it afterwards. This could be an issue if multiple
+         devices/streams need to read the same array but this shouldn't be an issue.
+
+  Template params:
+    IndexType - the type of the array used to store the index values. This must be an integral type
+
+    T - the type of input_array
+
+    BLOCK_SIZE_ - The number of threads in a block
+
+    BLOCKS_PER_ITEM_ - The number of blocks launches for each item in a row. eg. For a beam search topk, this could be the number
+                      of blocks needed to process a beam.
+
+    getRowOffsets - A boolean indicating whether we want the indices returned to be relative to the start of a row or relative to 
+                    the base pointer of the array.
+  
+  Function params:
+    input_array - The input matrix to the topk operator
+
+    topk_tmp_id_buf - Stores the intermediate indicies for the next phase of the kernel. Must be at least #rows * k * k * BLOCKS_PER_ITEM_
+
+    topk_tmp_val_buf - Stores the intermediate values for the next phase of the kernel. Must be at least #rows * items_per_row * k * BLOCKS_PER_ITEM_
+
+    Note:              The shape for the above items comes from the fact that each block launched finds the top k items for the given item in its row.
+                       So, for each item in a row, all the BLOCKS_PER_ITEM_ blocks write out k values. These are further reduced in stage 2.
+    
+    k - the k in top k. Specifies that in each row, the k largest/smallest elements should be retrieved
+
+    values_per_item - the number of values within each item in a given row. Eg. If a row has two items, and the row length is 100, the values_per_item would be 50.
+                      For beam search, this would be the vocabulary size and the items_per_row would be the number of hypotheses in the beam.
+    
+    descendingOrder - If true, the k largest elements are returned. Otherwise, the k smallest elements are returned.
+*/
+template<typename IndexType, typename T, int BLOCK_SIZE_, int BLOCKS_PER_ITEM_, bool getRowOffsets>
+__global__ void topk_stage_1(T* input_array, 
                              IndexType* topk_tmp_id_buf,
                              T* topk_tmp_val_buf, 
                              const int k, 
-                             const int vocab_size,
+                             const int values_per_item,
                              const int descendingOrder) {
 
+  // Set up shared memory needed for CUB reductions
   typedef cub::BlockReduce<TopK<IndexType, T>, BLOCK_SIZE_> BlockReduce;
   __shared__ typename BlockReduce::TempStorage temp_storage;
-
+  
+  // Compute constants used within each block. 
   const IndexType tid = threadIdx.x;
   const IndexType bid = blockIdx.x;
-  const IndexType row_id = bid / BLOCKS_PER_BEAM_; // row id for log_probs
-  const IndexType block_lane = bid % BLOCKS_PER_BEAM_; // block id for a beam 
-  const IndexType tmp_log_buf_index = row_id * vocab_size; 
-  const IndexType tmp_topk_buf_index = row_id * BLOCKS_PER_BEAM_ * k + block_lane * k;
-  TopK<IndexType, T> partial;
+  const IndexType row_id = bid / BLOCKS_PER_ITEM_; // row id for input_array
+  const IndexType block_lane = bid % BLOCKS_PER_ITEM_; // block id for a beam 
   const T minimal = descendingOrder? -FpInfinity<T>::infinity() : FpInfinity<T>::infinity();;
 
-  for(int ite = 0; ite < k; ite++) {
+  // Computes the index offset from the temp id and value arrays that each block should write its tmp output
+  const IndexType tmp_log_buf_index = row_id * values_per_item; 
+  const IndexType tmp_topk_buf_index = row_id * BLOCKS_PER_ITEM_ * k + block_lane * k;
+
+  // The partial topk result (reductions from global memory before performing block reduction)
+  TopK<IndexType, T> partial;
+
+  // Finds the largest/smallest element within a block's given range then blanks out that value in the input array 
+  // before starting the next iteration. The blanked out values are fixed after this loop.
+  for (int ite = 0; ite < k; ite++) {
     partial.init(descendingOrder);
     const IndexType threadStart = tid + block_lane * BLOCK_SIZE_;
 
     // This is needed to ensure the indices for the threads in each valid block starts in a valid range for that block.
-    if(threadStart < vocab_size) partial.index = threadStart;
+    if (threadStart < values_per_item) 
+      partial.index = threadStart;
+
+    // Each block constructs its partial result before performing a block level reduce.
     #pragma unroll
-    for(IndexType elem_id = threadStart; elem_id < vocab_size; elem_id += BLOCK_SIZE_ * BLOCKS_PER_BEAM_) {
+    for (IndexType elem_id = threadStart; elem_id < values_per_item; elem_id += BLOCK_SIZE_ * BLOCKS_PER_ITEM_) {
       IndexType index = elem_id + tmp_log_buf_index;
-      descendingOrder? partial.updateIfLarger(log_probs[index], index) : partial.updateIfSmaller(log_probs[index], index);
+      descendingOrder? partial.updateIfLarger(input_array[index], index) : partial.updateIfSmaller(input_array[index], index);
     }
 
+    // Invoke CUB to perform the block level reduce. The reduction function depends on the descending order flag.
     TopK<IndexType, T> total = BlockReduce(temp_storage).Reduce(partial, descendingOrder? reduce_topk_max<IndexType, T>: reduce_topk_min<IndexType, T>);
 
+    // Wrtie the index and value to global memory and blank out the max/min value found from the input_array so it is not considered in the next
+    // iteration.
     if (tid == 0) {
       const int index = tmp_topk_buf_index + ite;
       topk_tmp_id_buf[index] = getRowOffsets? total.index - tmp_log_buf_index : total.index;
       topk_tmp_val_buf[index] = total.value;
       // If we found a max, blank out the value in the log prob array before starting the next iteration.
       // Otherwise, we don't need to issue a write since all prob values must have been T::min()
-      if(total.value != minimal) log_probs[total.index] = minimal;
+      if (total.value != minimal) 
+        input_array[total.index] = minimal;
     }
     __syncthreads();
   }
 
   // Update prob array with original values.
-  for(int beam = tid; beam < k; beam += BLOCK_SIZE_) {
+  for (int beam = tid; beam < k; beam += BLOCK_SIZE_) {
     const IndexType index = tmp_topk_buf_index + beam;
     T val = topk_tmp_val_buf[index];
     // We only want to replace the value in the log prob array if a value was blanked out (we found a max).
     // When a max isn't found, topk_tmp_val_buf[index] will be T::min()
-    if(val != minimal) {
+    if (val != minimal) {
       IndexType k_idx = getRowOffsets? topk_tmp_id_buf[index] + tmp_log_buf_index : topk_tmp_id_buf[index];
-      log_probs[k_idx] = (T)topk_tmp_val_buf[index];
+      input_array[k_idx] = (T)topk_tmp_val_buf[index];
     } 
   }
 }
 
-template<typename IndexType, typename T, int BLOCK_SIZE_, int BLOCKS_PER_BEAM_>
+template<typename IndexType, typename T, int BLOCK_SIZE_, int BLOCKS_PER_ITEM_>
 __global__ void topk_stage_2(const IndexType* __restrict topk_tmp_id_buf,
                              T* topk_tmp_val_buf,
                              TopK<IndexType, T>* top,
@@ -173,7 +233,7 @@ __global__ void topk_stage_2(const IndexType* __restrict topk_tmp_id_buf,
                              const int k,
                              bool descendingOrder) {
 
-  const int size = beams_per_batch * k * BLOCKS_PER_BEAM_; 
+  const int size = beams_per_batch * k * BLOCKS_PER_ITEM_; 
   const int tid = threadIdx.x;
   const int batch_id = blockIdx.x;
   const T minimal = descendingOrder? -FpInfinity<T>::infinity() : FpInfinity<T>::infinity();;
@@ -213,14 +273,14 @@ __global__ void topk_stage_2(const IndexType* __restrict topk_tmp_id_buf,
   }
 }
 
-#define CASE_K(K,BLOCK_SIZE_1_, BLOCK_SIZE_2_, BLOCKS_PER_BEAM_) \
+#define CASE_K(K,BLOCK_SIZE_1_, BLOCK_SIZE_2_, BLOCKS_PER_ITEM_) \
   case K: \
-    topk_stage_1<IndexType, T, BLOCK_SIZE_1_, BLOCKS_PER_BEAM_, getRowOffsets><<<batch_size * beams_per_batch * BLOCKS_PER_BEAM_, BLOCK_SIZE_1_, 0, stream>>>( \
-        log_probs, \
+    topk_stage_1<IndexType, T, BLOCK_SIZE_1_, BLOCKS_PER_ITEM_, getRowOffsets><<<batch_size * beams_per_batch * BLOCKS_PER_ITEM_, BLOCK_SIZE_1_, 0, stream>>>( \
+        input_array, \
         topk_tmp_id_buf, \
         topk_tmp_val_buf, \
-        k, vocab_size, descendingOrder); \
-    topk_stage_2<IndexType, T, BLOCK_SIZE_2_, BLOCKS_PER_BEAM_><<<batch_size, BLOCK_SIZE_2_, K * sizeof(TopK<IndexType, T>), stream>>>( \
+        k, values_per_item, descendingOrder); \
+    topk_stage_2<IndexType, T, BLOCK_SIZE_2_, BLOCKS_PER_ITEM_><<<batch_size, BLOCK_SIZE_2_, K * sizeof(TopK<IndexType, T>), stream>>>( \
         topk_tmp_id_buf, \
         topk_tmp_val_buf, \
         tops, \
@@ -235,14 +295,14 @@ __global__ void topk_stage_2(const IndexType* __restrict topk_tmp_id_buf,
 // element.cu is removed. When this flag is true, the indices returns are the offsets within the row. When the flag is false, the indices
 // returned are offset from the base pointer.
 template <typename IndexType, typename T, bool getRowOffsets=false>
-void topK_kernelLauncher(T* log_probs,
+void topK_kernelLauncher(T* input_array,
                          IndexType* topk_tmp_id_buf,
                          T* topk_tmp_val_buf,
                          TopK<IndexType, T>* tops,
                          const int batch_size,
                          const int beams_per_batch,
                          const int k,
-                         const int vocab_size,
+                         const int values_per_item,
                          bool descendingOrder,
                          cudaStream_t stream) {
   
@@ -259,11 +319,11 @@ void topK_kernelLauncher(T* log_probs,
     CASE_K(32,256,128,1);
     CASE_K(64,256,256,1);
     default:
-      topk_stage_1<IndexType, T, 128, 1, getRowOffsets><<<batch_size * beams_per_batch * 1, 128, 0, stream>>>(log_probs, 
+      topk_stage_1<IndexType, T, 128, 1, getRowOffsets><<<batch_size * beams_per_batch * 1, 128, 0, stream>>>(input_array, 
                                                                                                topk_tmp_id_buf, 
                                                                                                topk_tmp_val_buf,
                                                                                                k, 
-                                                                                               vocab_size, 
+                                                                                               values_per_item, 
                                                                                                descendingOrder);
 
       topk_stage_2<IndexType, T, 128, 1><<<batch_size, 128, k * sizeof(TopK<IndexType, T>), stream>>>(topk_tmp_id_buf,
@@ -279,7 +339,7 @@ void topK_kernelLauncher(T* log_probs,
 }
 
 template <typename IndexType, typename T, bool getRowOffsets=false>
-void topK_kernelLauncher(T* log_probs,
+void topK_kernelLauncher(T* input_array,
                          IndexType* topk_tmp_id_buf,
                          T* topk_tmp_val_buf,
                          IndexType* outIndices,
@@ -287,7 +347,7 @@ void topK_kernelLauncher(T* log_probs,
                          const int batch_size,
                          const int beams_per_batch,
                          const int k,
-                         const int vocab_size,
+                         const int values_per_item,
                          bool descendingOrder,
                          cudaStream_t stream) {
   
@@ -303,11 +363,11 @@ void topK_kernelLauncher(T* log_probs,
     CASE_K(32,256,128,1);
     CASE_K(64,256,256,1);
     default:
-      topk_stage_1<IndexType, T, 128, 1, getRowOffsets><<<batch_size * beams_per_batch * 1, 128, 0, stream>>>(log_probs, 
+      topk_stage_1<IndexType, T, 128, 1, getRowOffsets><<<batch_size * beams_per_batch * 1, 128, 0, stream>>>(input_array, 
                                                                                                topk_tmp_id_buf, 
                                                                                                topk_tmp_val_buf,
                                                                                                k, 
-                                                                                               vocab_size, 
+                                                                                               values_per_item, 
                                                                                                descendingOrder);
 
       topk_stage_2<IndexType, T, 128, 1><<<batch_size, 128, k * sizeof(TopK<IndexType, T>), stream>>>(topk_tmp_id_buf,

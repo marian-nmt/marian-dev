521f6343cb9ea86549e15cd11d0d670dd1ddbc7c
Kenneth Heafield <github@kheafield.com>
2021-03-28 18:09:20 +0000

    Model and GPUSlot separation, add vocab support

--- src/command/marian_swapper.cpp
+++ src/command/marian_swapper.cpp
@@ -9,130 +9,142 @@
 #include <vector>
 #include "tensors/gpu/swap.h"
 namespace marian {
-class SwapperTranslator {
-    private:
-        Ptr<Options> opts_;
-        Ptr<ExpressionGraph> graph_;
-        Ptr<Scorer> scorer_;
 
-        std::vector<Ptr<Vocab>> srcVocabs_;
-        Ptr<Vocab> trgVocab_;
+/* A model loaded on the CPU and possibly on a GPU */
+class Model {
+  private:
+    std::vector<io::Item> parameters_;
+    std::vector<Ptr<Vocab>> srcVocabs_;
+    Ptr<Vocab> trgVocab_;
 
-        // Models to store model;
-        bool primary_ = true;
-        std::vector<io::Item> primaryModel_;
-        std::vector<io::Item> secondaryModel_;
-
-    std::vector<io::Item> prepareItem(std::string path){
-        std::vector<io::Item> ret = io::loadItems(path);
-        // Find the special element and remove it:
-        size_t special_idx = 0;
-        for (size_t i = 0; i < ret.size(); i++) {
-            if (ret[i].name == "special:model.yml") {
-                special_idx = i;
-                break;
-            }
+  public:
+    Model(Ptr<Options> options, const std::string &parameters, const std::vector<std::string> &sourceVocabPaths, const std::string &targetVocabPath)
+        : parameters_(io::loadItems(parameters)) {
+      // Load parameters.
+      // Find the special element and remove it:
+      size_t special_idx = 0;
+      for (size_t i = 0; i < parameters_.size(); i++) {
+        if (parameters_[i].name == "special:model.yml") {
+          special_idx = i;
+          break;
         }
-        ret.erase(ret.begin() + special_idx);
-        // Prepare the name so that it matches the named map
-        for (auto&& item : ret) {
-            item.name = "F0::" + item.name;
-        }
-        return ret;
+      }
+      parameters_.erase(parameters_.begin() + special_idx);
+      // Prepare the name so that it matches the named map
+      for (auto&& item : parameters_) {
+        item.name = "F0::" + item.name;
+      }
+
+      // Load source vocabs.
+      const std::vector<int> &maxVocabs = options->get<std::vector<int>>("dim-vocabs");
+      for(size_t i = 0; i < sourceVocabPaths.size(); ++i) {
+        Ptr<Vocab> vocab = New<Vocab>(options, i);
+        vocab->load(sourceVocabPaths[i], maxVocabs[i]);
+        srcVocabs_.emplace_back(vocab);
+      }
+
+      // Load target vocab.
+      trgVocab_ = New<Vocab>(options, sourceVocabPaths.size());
+      trgVocab_->load(targetVocabPath);
     }
 
-    public:
-    SwapperTranslator(Ptr<Options> opt) : opts_(opt),
-                                          primaryModel_(prepareItem(opt->get<std::vector<std::string>>("models")[0])),
-                                          secondaryModel_(prepareItem(opt->get<std::string>("swap-model"))) {
-        opts_->set("inference", true);
-        opts_->set("shuffle", "none");
+    const std::vector<io::Item> &Parameters() const { return parameters_; }
 
-        // Get vocabs
-        auto vocabPaths = opts_->get<std::vector<std::string>>("vocabs");
-        std::vector<int> maxVocabs = opts_->get<std::vector<int>>("dim-vocabs");
+    const std::vector<Ptr<Vocab>> &SrcVocabs() const { return srcVocabs_; }
 
-        for(size_t i = 0; i < vocabPaths.size() - 1; ++i) {
-            Ptr<Vocab> vocab = New<Vocab>(opts_, i);
-            vocab->load(vocabPaths[i], maxVocabs[i]);
-            srcVocabs_.emplace_back(vocab);
-        }
+    Ptr<Vocab> TrgVocab() const { return trgVocab_; }
+};
 
-        trgVocab_ = New<Vocab>(opts_, vocabPaths.size() - 1);
-        trgVocab_->load(vocabPaths.back());
+/* Reserved space on a GPU with which to translate */
+class GPUSlot {
+	private:
+    Ptr<Options> options_;
+    Ptr<ExpressionGraph> graph_;
+    std::vector<Ptr<Scorer> > scorers_;
 
-        // get device IDs
-        auto devices = Config::getDevices(opts_);
-        auto numDevices = devices.size();
-        std::cerr << "Num devices: " << numDevices << std::endl;
+    // Last model used for translation.  Used to skip loading.
+    const Model *loadedModel_;
+
+    void Load(const std::vector<io::Item> &parameters) {
+      timer::Timer timer;
+      auto namedMap = graph_->getParamsNamedMap();
+      for (auto&& item : parameters) {
+        auto to = reinterpret_cast<char *>(namedMap[item.name]->val()->memory()->data());
+        swapper::copyCpuToGpu(to, &item.bytes[0], item.bytes.size());
+      }
+      LOG(info, "Load took: {:.8f}s wall", timer.elapsed());
+    }
+
+  public:
+    explicit GPUSlot(Ptr<Options> options) : options_(options), loadedModel_(nullptr) {
+      options_->set("inference", true);
+      options_->set("shuffle", "none");
+      // get device IDs
+      auto devices = Config::getDevices(options_);
+      auto numDevices = devices.size();
+      std::cerr << "Num devices: " << numDevices << std::endl;
         
-        // Create graph
-        graph_ = New<ExpressionGraph>();
-        auto prec = opts_->get<std::vector<std::string>>("precision", {"float32"});
-        graph_->setDefaultElementType(typeFromString(prec[0]));
-        graph_->setDevice(devices[0]);
-        graph_->reserveWorkspaceMB(opts_->get<size_t>("workspace"));
-        scorer_ = createScorers(opts_)[0];
-        scorer_->init(graph_);
-        graph_->forward();
+      // Create graph
+      graph_ = New<ExpressionGraph>();
+      auto prec = options_->get<std::vector<std::string>>("precision", {"float32"});
+      graph_->setDefaultElementType(typeFromString(prec[0]));
+      graph_->setDevice(devices[0]);
+      graph_->reserveWorkspaceMB(options_->get<size_t>("workspace"));
+      // TODO: multiple scorers.
+      Ptr<Scorer> scorer = createScorers(options_)[0];
+      scorer->init(graph_);
+      scorers_.push_back(scorer);
+      graph_->forward();
     }
 
-    void translateTxt(std::string txt) {
-        std::vector<std::string> instr(1, txt);
-        auto corpus_ = New<data::TextInput>(instr, srcVocabs_, opts_);
-        data::BatchGenerator<data::TextInput> batchGenerator(corpus_, opts_, nullptr, false);
+    void Translate(const Model &model, const std::vector<std::string> &input) {
+      if (loadedModel_ != &model) {
+        Load(model.Parameters());
+        loadedModel_ = &model;
+      }
+      auto corpus = New<data::TextInput>(input, model.SrcVocabs(), options_);
+      data::BatchGenerator<data::TextInput> batchGenerator(corpus, options_, nullptr, false);
 
-        static const std::vector<Ptr<Scorer> > scorers(1, scorer_);
-        auto search = New<BeamSearch>(opts_, scorers, trgVocab_);
-        auto printer = New<OutputPrinter>(opts_, trgVocab_);
-        static int i = 0;
-        for (auto&& batch : batchGenerator) {
-            auto histories = search->search(graph_, batch);
-            for(auto history : histories) {
-                std::stringstream best1;
-                std::stringstream bestn;
-                printer->print(history, best1, bestn);
-                LOG(info, "Translation {} : {}", i, best1.str());
-                i++;
-            }
+      auto search = New<BeamSearch>(options_, scorers_, model.TrgVocab());
+      auto printer = New<OutputPrinter>(options_, model.TrgVocab());
+      for (auto&& batch : batchGenerator) {
+        auto histories = search->search(graph_, batch);
+        for(auto history : histories) {
+          std::stringstream best1;
+          std::stringstream bestn;
+          printer->print(history, best1, bestn);
+          LOG(info, "Translation {}", best1.str());
         }
-    }
-
-    void swapActual(std::vector<io::Item>& from) {
-        auto namedMap = graph_->getParamsNamedMap();
-        for (auto&& item : from) {
-            auto to = reinterpret_cast<char *>(namedMap[item.name]->val()->memory()->data());
-            swapper::copyCpuToGpu(to, &item.bytes[0], item.bytes.size());
-        }
-    }
-
-    void swap() {
-        timer::Timer timer;
-        if (primary_) {
-            swapActual(secondaryModel_);
-            primary_ = false;
-        } else {
-            swapActual(primaryModel_);
-            primary_ = true;
-        }
-        LOG(info, "Swap took: {:.8f}s wall", timer.elapsed());
+      }
     }
 };
+
 } // namespace marian
 
+/* Demo program */
 int main(int argc, char** argv) {
   using namespace marian;
-  auto options = parseOptions(argc, argv, cli::mode::translation);
-  SwapperTranslator swapper(options);
+  Ptr<Options> options = parseOptions(argc, argv, cli::mode::translation);
+  GPUSlot slot(options);
+  Model pten(options,
+      "/home/ubuntu/consistent-big-models/padded/pten.npz",
+      {"/home/ubuntu/consistent-big-models/padded/pten.vocab"},
+      "/home/ubuntu/consistent-big-models/padded/pten.vocab");
 
+  Model enit(options,
+      "/home/ubuntu/consistent-big-models/padded/enit.npz",
+      {"/home/ubuntu/consistent-big-models/padded/enit.vocab"},
+      "/home/ubuntu/consistent-big-models/padded/enit.vocab");
+
+  const Model *model = &pten;
   std::string line;
   while (std::getline(std::cin, line)) {
-    if (line == "quit") {
-        break;
-    } else if (line == "swap") {
-        swapper.swap();
+    if (line == " TRANSLATE PTEN") {
+      model = &pten;
+    } else if (line == " TRANSLATE ENIT") {
+      model = &enit;
     } else {
-        swapper.translateTxt(line);
+      slot.Translate(*model, {line});
     }
   }
 

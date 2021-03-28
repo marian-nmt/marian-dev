47feb2b2d103ab857985b3c05bff010b0beeddcc
Kenneth Heafield <github@kheafield.com>
2021-03-28 21:52:33 +0000

    Alignments

--- src/command/marian_swapper.cpp
+++ src/command/marian_swapper.cpp
@@ -1,41 +1,94 @@
-#include "translator/swappable.h"
+#include "translator/history.h"
 #include "translator/output_printer.h"
+#include "translator/swappable.h"
 
 #include <iostream>
 #include <string>
+#include <unordered_map>
+
+namespace marian {
+void LoadBig(Ptr<Options> options, std::unordered_map<std::string, SwappableModel> &to) {
+  to.emplace("pten", SwappableModel(options,
+      "/home/ubuntu/consistent-big-models/padded/pten.npz",
+      {"/home/ubuntu/consistent-big-models/padded/pten.vocab"},
+      "/home/ubuntu/consistent-big-models/padded/pten.vocab"));
+
+  to.emplace("iten", SwappableModel(options,
+      "/home/ubuntu/consistent-big-models/padded/enit.npz",
+      {"/home/ubuntu/consistent-big-models/padded/enit.vocab"},
+      "/home/ubuntu/consistent-big-models/padded/enit.vocab"));
+}
+
+void LoadTiny(Ptr<Options> options, std::unordered_map<std::string, SwappableModel> &to) {
+  std::vector<std::string> models = {"csen", "encs", "enet", "eten", "esen", "enes"};
+  for (const std::string m : models) {
+    std::string base = "/home/ubuntu/consistent-bergamot-students/padded/";
+    base += m + ".";
+    to.emplace(m, SwappableModel(options, base + "npz", {base + "spm"}, base + "spm"));
+  }
+}
+
+} // namespace
 
 /* Demo program: run with options for any of the models */
 int main(int argc, char** argv) {
   using namespace marian;
   Ptr<Options> options = parseOptions(argc, argv, cli::mode::translation);
+  // You can have multiple slots.  In principle these can even have different sizes, just use separate options.
   SwappableSlot slot(options);
-  SwappableModel pten(options,
-      "/home/ubuntu/consistent-big-models/padded/pten.npz",
-      {"/home/ubuntu/consistent-big-models/padded/pten.vocab"},
-      "/home/ubuntu/consistent-big-models/padded/pten.vocab");
+  
+  std::unordered_map<std::string, SwappableModel> models;
+//  LoadBig(options, models);
+  LoadTiny(options, models);
 
-  SwappableModel enit(options,
-      "/home/ubuntu/consistent-big-models/padded/enit.npz",
-      {"/home/ubuntu/consistent-big-models/padded/enit.vocab"},
-      "/home/ubuntu/consistent-big-models/padded/enit.vocab");
+  // begin with a space to avoid conflict with a real sentence.
+  const std::string kSwitchPrefix(" CHANGE ");
 
-  const SwappableModel *model = &pten;
+  bool alignments = !options->get<std::string>("alignment").empty();
+
+  const SwappableModel *model = nullptr;
   std::string line;
   while (std::getline(std::cin, line)) {
-    if (line == " TRANSLATE PTEN") {
-      model = &pten;
-      continue;
-    } else if (line == " TRANSLATE ENIT") {
-      model = &enit;
+    // Switch out which model is used.
+    if (line.substr(0, kSwitchPrefix.size()) == kSwitchPrefix) {
+      std::string key = line.substr(kSwitchPrefix.size());
+      auto found = models.find(key);
+      if (found == models.end()) {
+        std::cerr << "Model for " << key << " not loaded." << std::endl;
+        return 1;
+      }
+      model = &found->second;
       continue;
     }
-    marian::OutputPrinter printer(options, model->TrgVocab());
+    if (!model) {
+      std::cerr << "Select a model first." << std::endl;
+      continue;
+    }
+
+    // Actually translating with a model.
     marian::Histories histories = slot.Translate(*model, {line});
+    // In practice there is one history because we provided one line.
     for(auto history : histories) {
-      std::stringstream best1;
-      std::stringstream bestn;
-      printer.print(history, best1, bestn);
-      std::cout << best1.str() << '\n';
+      Result result(history->top());
+      Words words = std::get<0>(result);
+      std::cout << model->TrgVocab()->decode(words) << std::endl;
+
+      /* Print alignments */
+      if (alignments) {
+        Hypothesis &hypo = *std::get<1>(result);
+        // [t][s] -> P(s|t)
+        marian::data::SoftAlignment alignment(hypo.tracebackAlignment());
+        // An easier call for this is:
+        // std:cout << data::SoftAlignToString(alignment);
+        // The below is just there to show how access them programatically.
+        // NB you can convert to hard with data::ConvertSoftAlignToHardAlign(alignment, threshold)
+        for (auto target : alignment) {
+          for (float source : target) {
+            std::cout << source << ' ';
+          }
+          std::cout << '\n';
+        }
+      }
     }
   }
 

cf12178df736f25ba256ba4cd347f5afd542234e
Nikolay Bogoychev <nheart@gmail.com>
2021-03-30 14:25:41 +0100

    Allow CPU only compilation

--- src/tensors/gpu/swap.h
+++ src/tensors/gpu/swap.h
@@ -1,6 +1,14 @@
+#pragma once
 #include <stdlib.h>
+#include "common/logging.h"
 namespace marian {
     namespace swapper {
+#ifdef CUDA_FOUND
         void copyCpuToGpu(char * gpuOut, const char * in, size_t count);
+#else
+        inline void copyCpuToGpu(char * gpuOut, const char * in, size_t count) {
+            ABORT("Copy from CPU to GPU memory is only available with CUDA.");
+        }
+#endif
     }
 }

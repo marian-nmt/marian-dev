29415c71f892d65ee7a025617242fee3dcb25bf7
Rihards Krišlauks <rihards.krislauks@tilde.lv>
2021-04-19 11:32:36 +0300

    Make a copy of all the swappable stuff to later adjust for training

--- src/translator/swappable.cpp
+++ src/translator/swappable.cpp
@@ -11,6 +11,152 @@
 #include "tensors/gpu/swap.h"
 
 namespace marian {
+std::string MultilineInputHack(const std::vector<std::string> &input) {
+  if (input.size() == 1) {
+    return input[0];
+  } else {
+    std::string ret;
+    std::size_t size = 0;
+    for (auto&& line : input) {
+      size += line.size() + 1;
+    }
+    ret.reserve(size);
+    for (auto&& line : input) {
+      ret.append(line);
+      ret.append("\n");
+    }
+    return ret;
+  }
+}
+
+namespace {
+  DeviceId LookupGPU(const Ptr<Options> options, size_t deviceIdx) {
+    auto devices = Config::getDevices(options);
+    ABORT_IF(deviceIdx >= devices.size(), "GPU device index higher than configured.");
+    return devices[deviceIdx];
+  }
+} // namespace
+
+void GPUEngineTrain::SwapPointers(std::vector<MemoryPiece::PtrType> &with) {
+  auto write_it = graph_->params()->begin();
+  auto read_it = with.begin();
+  for (; read_it != with.end(); ++write_it, ++read_it) {
+    std::swap(*(*write_it)->val()->memory(), **read_it);
+  }
+}
+
+GPUEngineTrain::GPUEngineTrain(Ptr<Options> options, size_t deviceIdx) 
+  : options_(options), graph_(New<ExpressionGraph>()), myDeviceId_(LookupGPU(options, deviceIdx)), allocator_(myDeviceId_, 0, 128 * 1048576) {
+  ABORT_IF(myDeviceId_.type == DeviceType::cpu, "Swappable slot only works for GPU devices.");
+  options_->set("inference", true);
+  options_->set("shuffle", "none");
+
+  // Create graph
+  auto prec = options_->get<std::vector<std::string>>("precision", {"float32"});
+  graph_->setDefaultElementType(typeFromString(prec[0]));
+  graph_->setDevice(myDeviceId_);
+  graph_->reserveWorkspaceMB(options_->get<size_t>("workspace"));
+
+  scorers_ = createScorers(options_);
+  for (auto scorer : scorers_) {
+    scorer->init(graph_);
+    // TODO lexical shortlists are not supported yet.
+  }
+  graph_->forward();
+  // TODO: reach into graph_->params() private members and free the parameter memory.
+}
+
+GPUEngineTrain::~GPUEngineTrain() {}
+
+GPULoadedModelTrain::GPULoadedModelTrain(Ptr<GPUEngineTrain> gpu) : engine_(gpu) {
+  for (auto &param : *engine_->graph_->params()) {
+    parameters_.push_back(engine_->allocator_.alloc(param->val()->memory()->size()));
+  }
+}
+
+GPULoadedModelTrain::~GPULoadedModelTrain() {
+  for (MemoryPiece::PtrType &p : parameters_) {
+    engine_->allocator_.free(p);
+  }
+}
+
+void GPULoadedModelTrain::Load(const GPULoadedModelTrain &from) {
+  srcVocabs_ = from.srcVocabs_;
+  trgVocab_ = from.trgVocab_;
+
+  ABORT_IF(engine_ != from.engine_, "TODO: copy across GPUs.");
+
+  for (size_t i = 0; i < parameters_.size(); ++i) {
+    swapper::copyGpuToGpu(reinterpret_cast<char*>(parameters_[i]->data()), reinterpret_cast<const char*>(from.parameters_[i]->data()), parameters_[i]->size(), engine_->myDeviceId_);
+  }
+}
+
+void GPULoadedModelTrain::Load(const CPULoadedModelTrain &from) {
+  srcVocabs_ = from.SrcVocabs();
+  trgVocab_ = from.TrgVocab();
+  for (size_t i = 0; i < parameters_.size(); ++i) {
+    swapper::copyCpuToGpu(reinterpret_cast<char*>(parameters_[i]->data()), from.Parameters()[i].data(), from.Parameters()[i].size(), engine_->myDeviceId_);
+  }
+}
+
+Histories GPULoadedModelTrain::Translate(const std::vector<std::string> &input) {
+  ABORT_IF(!trgVocab_, "GPULoadedModelTrain needs to be overwritten by a CPU model first.");
+  engine_->SwapPointers(parameters_);
+
+  auto corpus = New<data::TextInput>(std::vector<std::string>(1, MultilineInputHack(input)), srcVocabs_, engine_->options_); // @TODO dirty hack
+  data::BatchGenerator<data::TextInput> batchGenerator(corpus, engine_->options_, nullptr, false); // @TODO if the asynchronous batch preparation = true, but we supply less text than the mini-batch size we crash
+
+  BeamSearch search(engine_->options_, engine_->scorers_, trgVocab_);
+  Histories ret;
+  ret.reserve(input.size());
+  for (auto&& batch : batchGenerator) {
+    auto result = search.search(engine_->graph_, batch);
+    ret.insert(ret.end(), result.begin(), result.end());
+  }
+  std::sort(ret.begin(), ret.end(),[](marian::Ptr<marian::History> a, marian::Ptr<marian::History> b){return a->getLineNum() < b->getLineNum();});
+  engine_->SwapPointers(parameters_);
+  return ret;
+}
+
+CPULoadedModelTrain::CPULoadedModelTrain(Ptr<Options> options, const std::string &parameters, const std::vector<std::string> &sourceVocabPaths, const std::string &targetVocabPath)
+  : parameters_(io::loadItems(parameters)) {
+  // Load parameters.
+  // Find the special element and remove it:
+  size_t special_idx = 0;
+  for (size_t i = 0; i < parameters_.size(); i++) {
+    if (parameters_[i].name == "special:model.yml") {
+      special_idx = i;
+      break;
+    }
+  }
+  parameters_.erase(parameters_.begin() + special_idx);
+  // Prepare the name so that it matches the named map
+  for (auto&& item : parameters_) {
+    item.name = "F0::" + item.name;
+  }
+  // Sort by name to match params order.
+  std::sort(parameters_.begin(), parameters_.end(), [](const io::Item &a, const io::Item &b){return a.name < b.name;});
+
+  // Load source vocabs.
+  const std::vector<int> &maxVocabs = options->get<std::vector<int>>("dim-vocabs");
+  for(size_t i = 0; i < sourceVocabPaths.size(); ++i) {
+    Ptr<Vocab> vocab = New<Vocab>(options, i);
+    vocab->load(sourceVocabPaths[i], maxVocabs[i]);
+    srcVocabs_.emplace_back(vocab);
+  }
+
+  // Load target vocab.
+  trgVocab_ = New<Vocab>(options, sourceVocabPaths.size());
+  trgVocab_->load(targetVocabPath);
+}
+
+
+
+  // ##### ^ above is stuff for runtime domain adaptation
+
+
+
+
 
 void GPUEngine::SwapPointers(std::vector<MemoryPiece::PtrType> &with) {
   auto write_it = graph_->params()->begin();
@@ -19,14 +165,6 @@ void GPUEngine::SwapPointers(std::vector<MemoryPiece::PtrType> &with) {
     std::swap(*(*write_it)->val()->memory(), **read_it);
   }
 }
-
-namespace {
-DeviceId LookupGPU(const Ptr<Options> options, size_t deviceIdx) {
-  auto devices = Config::getDevices(options);
-  ABORT_IF(deviceIdx >= devices.size(), "GPU device index higher than configured.");
-  return devices[deviceIdx];
-}
-} // namespace
 
 GPUEngine::GPUEngine(Ptr<Options> options, size_t deviceIdx) 
   : options_(options), graph_(New<ExpressionGraph>()), myDeviceId_(LookupGPU(options, deviceIdx)), allocator_(myDeviceId_, 0, 128 * 1048576) {
@@ -79,24 +217,6 @@ void GPULoadedModel::Load(const CPULoadedModel &from) {
   trgVocab_ = from.TrgVocab();
   for (size_t i = 0; i < parameters_.size(); ++i) {
     swapper::copyCpuToGpu(reinterpret_cast<char*>(parameters_[i]->data()), from.Parameters()[i].data(), from.Parameters()[i].size(), engine_->myDeviceId_);
-  }
-}
-
-std::string MultilineInputHack(const std::vector<std::string> &input) {
-  if (input.size() == 1) {
-    return input[0];
-  } else {
-    std::string ret;
-    std::size_t size = 0;
-    for (auto&& line : input) {
-      size += line.size() + 1;
-    }
-    ret.reserve(size);
-    for (auto&& line : input) {
-      ret.append(line);
-      ret.append("\n");
-    }
-    return ret;
   }
 }
 
--- src/translator/swappable.h
+++ src/translator/swappable.h
@@ -14,10 +14,87 @@
 #include <vector>
 namespace marian {
 
+class GPULoadedModelTrain;
+class CPULoadedModelTrain;
+
 class Scorer;
 
 class GPULoadedModel;
 class CPULoadedModel;
+
+
+/* Execute on a particular device */
+class GPUEngineTrain {
+private:
+  friend class GPULoadedModelTrain;
+  Ptr<Options> options_;
+  Ptr<ExpressionGraph> graph_;
+  std::vector<Ptr<Scorer> > scorers_;
+  const DeviceId myDeviceId_;
+  Allocator allocator_;
+
+  void SwapPointers(std::vector<MemoryPiece::PtrType> &with);
+
+public:
+  /**
+    * @param options The marian options object
+    * @param deviceNum The index of the device you want to use for this slot. Note that this is not the deviceID but the index of the device in the
+    *                  array of supplied devices. Eg if you provide -d 0 3 5 and you want the Slot to run on GPU 3, you provide deviceNum=1.
+    */
+  explicit GPUEngineTrain(Ptr<Options> options, size_t deviceNum);
+
+  ~GPUEngineTrain();
+};
+
+/* A model loaded on the GPU that can be overwritten from CPU or GPU. */
+class GPULoadedModelTrain {
+  private:
+    Ptr<GPUEngineTrain> engine_;
+
+    std::vector<MemoryPiece::PtrType> parameters_;
+    std::vector<Ptr<Vocab>> srcVocabs_;
+    Ptr<Vocab> trgVocab_;
+
+  public:
+    GPULoadedModelTrain(Ptr<GPUEngineTrain> gpu);
+
+    ~GPULoadedModelTrain();
+
+    const std::vector<Ptr<Vocab>> &SrcVocabs() const { return srcVocabs_; }
+
+    Ptr<Vocab> TrgVocab() const { return trgVocab_; }
+
+    // Overwrite this model with parameters from a different one.
+    void Load(const CPULoadedModelTrain &from);
+    void Load(const GPULoadedModelTrain &from);
+
+    Histories Translate(const std::vector<std::string> &input);
+};
+
+/* A model loaded on the CPU. */
+class CPULoadedModelTrain {
+  private:
+    std::vector<io::Item> parameters_;
+    std::vector<Ptr<Vocab>> srcVocabs_;
+    Ptr<Vocab> trgVocab_;
+
+  public:
+    // The parts of Options that relate to model and vocab are ignored.  The files provided will be loaded.
+    CPULoadedModelTrain(Ptr<Options> options, const std::string &parameters, const std::vector<std::string> &sourceVocabPaths, const std::string &targetVocabPath);
+
+    const std::vector<io::Item> &Parameters() const { return parameters_; }
+
+    const std::vector<Ptr<Vocab>> &SrcVocabs() const { return srcVocabs_; }
+
+    Ptr<Vocab> TrgVocab() const { return trgVocab_; }
+};
+
+
+
+// ##### ^ above is stuff for runtime domain adaptation
+
+
+
 
 /* Execute on a particular device */
 class GPUEngine {

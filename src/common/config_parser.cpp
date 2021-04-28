400ed834abe9327ee479ce87bdb54495790f050f
Graeme Nail <graemenail.work@gmail.com>
2021-04-28 13:51:19 +0100

    CLI option for model-mmap only for translation and CPU compile

--- src/common/config_parser.cpp
+++ src/common/config_parser.cpp
@@ -183,9 +183,12 @@ void ConfigParser::addOptionsModel(cli::CLIWrapper& cli) {
         "Path prefix for pre-trained model to initialize model weights");
     }
   }
-
-  cli.add<bool>("--model-mmap",
+#ifdef COMPILE_CPU
+  if(mode_ == cli::mode::translation) {
+    cli.add<bool>("--model-mmap",
       "Use memory-mapping when loading model (CPU only)");
+  }
+#endif
   cli.add<bool>("--ignore-model-config",
       "Ignore the model configuration saved in npz file");
   cli.add<std::string>("--type",

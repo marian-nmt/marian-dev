79002cbaee38251a8d70b81367e91a9077fd7f2d
Rihards Krišlauks <rihards.krislauks@tilde.lv>
2021-09-16 16:18:49 +0300

    Add documentation comments for adaptive context reader classes

--- src/translator/self_adaptive.h
+++ src/translator/self_adaptive.h
@@ -16,7 +16,12 @@ using namespace data;
 
 class AdaptiveContextReader;
 
-class AdaptiveContextIterator : public IteratorFacade<AdaptiveContextIterator, std::vector<std::string>> {
+/**
+ * @brief An iterator for easier access of the context sentences produced by
+ * `AdaptiveContextReader::getSamples()`
+ */
+class AdaptiveContextIterator
+  : public IteratorFacade<AdaptiveContextIterator, std::vector<std::string>> {
 private:
   AdaptiveContextReader* trainSetReader_;
   std::vector<std::string> currentSamples_;
@@ -34,16 +39,38 @@ public:
   void increment() override;
 };
 
+/**
+ * @brief Reads the context sentences, that are used for on-the-fly training in
+ * the self-adaptive translation mode, from files.
+ */
 class AdaptiveContextReader {
   std::vector<UPtr<io::InputFileStream>> files_;
+  /// Indicates whether the input files have been exhausted.
   bool eof_ = false;
 
 public:
+  /**
+   * @brief Initializes a new reader by supplying paths to the files with
+   * context sentences
+   *
+   * @param paths paths to the input files. The input files contain
+   * newline-separated parallel sentence pairs (as usual for MT). Sentences are
+   * grouped by the translatable sentences (which are provided in a different
+   * file). Each group is delimited by a single empty line. The sentence group
+   * can be empty (no context is provided for the respective translatable
+   * sentence) in which case it is also represented by a single empty line.
+   */
   AdaptiveContextReader(std::vector<std::string> paths) {
     for(auto& path : paths)
       files_.emplace_back(new io::InputFileStream(path));
   }
 
+  /**
+   * @brief Returns an iterator over the sets of context sentences produced by
+   * `getSamples()`
+   *
+   * @return the beginning of the iterator.
+   */
   AdaptiveContextIterator begin() {
     return AdaptiveContextIterator(this);
   }
@@ -56,13 +83,29 @@ public:
     return eof_;
   }
 
+  /**
+   * @brief Reads the next set of samples -- the contaxt sentences -- for
+   * on-the-fly training in the self-adaptive translation mode.
+   *
+   * @details The input files contain newline-separated parallel sentence pairs
+   * (as usual for MT). Sentences are grouped by the translatable sentences
+   * (which are provided in a different file). Each group is delimited by a
+   * single empty line. The sentence group can be empty (no context is provided
+   * for the respective translatable sentence) in which case it is also
+   * represented by a single empty line.
+   *
+   * @return a vector representing a single group of context sentences. Each
+   * element in the vector contains newline seperated input lines comming from a
+   * single file, e.g., [0] could contain 3 newline separated sentences in
+   * English and [1] would contain their 3 respective translations in Latvian.
+   */
   std::vector<std::string> getSamples() {
     // extracted lines for source and target corpora
     std::vector<std::string> samples;
     // counters of number of lines extracted for source and target
     std::vector<size_t> counts;
 
-    // Early exit if files are exhausted
+    // Early exit if input files are exhausted
     if (eof_) return samples;
 
     for(auto const& file : files_) {
@@ -115,7 +158,7 @@ void AdaptiveContextIterator::increment() {
   }
   // If we're at the end of the iterator and increment has been called yet another time, there's
   // a bug in the calling code
-  ABORT_IF(trainSetReader_ == nullptr, "Incrementing the end of the iterator isn't allowed");
+  ABORT_IF(trainSetReader_ == nullptr, "Incrementing past the end of the iterator isn't allowed");
 
   currentSamples_ = trainSetReader_->getSamples();
 }

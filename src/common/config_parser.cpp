7a9deacf7839edff7569337909ae64862f8e3190
Graeme Nail <graemenail.work@gmail.com>
2021-12-13 16:16:23 +0000

    Add shortlist as a command line option for marian-scorer

--- src/common/config_parser.cpp
+++ src/common/config_parser.cpp
@@ -738,6 +738,8 @@ void ConfigParser::addOptionsScoring(cli::CLIWrapper& cli) {
       "Paths to vocabulary files have to correspond to --train-sets. "
       "If this parameter is not supplied we look for vocabulary files source.{yml,json} and target.{yml,json}. "
       "If these files do not exists they are created");
+  cli.add<std::vector<std::string>>("--shortlist",
+     "Use softmax shortlist: path first best prune");
   cli.add<bool>("--n-best",
       "Score n-best list instead of plain text corpus");
   cli.add<std::string>("--n-best-feature",

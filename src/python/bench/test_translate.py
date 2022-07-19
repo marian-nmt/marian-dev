efdc1571861b03685988e41eda9312f560530ac7
Elijah Rippeth <elijah.rippeth@gmail.com>
2022-07-19 14:10:01 -0400

    add coarse benchmarking to test inference. (#949)

---
+++ src/python/bench/test_translate.py
@@ -0,0 +1,17 @@
+import sys
+import itertools
+
+from pymarian import Translator
+
+marian = Translator(sys.argv[1])
+
+def batch(s, batch_size=1600):
+    it = iter(s)
+    while True:
+        chunk = list(itertools.islice(it, batch_size))
+        if not chunk:
+            return
+        yield chunk
+
+for b in batch(map(str.strip, sys.stdin)):
+    print(marian.translate("\n".join(b)))
